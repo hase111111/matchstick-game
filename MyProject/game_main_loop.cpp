dc0c8863b41030a73d82c34ff7824607df8985e1
Hasegawa Taisei <hasehasehase61@gmail.com>
2024-07-23 03:43:36 +0900

    新規クラスに合わせてメインループを更新

--- MyProject/game_main_loop.cpp
+++ MyProject/game_main_loop.cpp
@@ -8,41 +8,74 @@
 
 namespace match_stick {
 
-GameMainLoop::GameMainLoop() : fps_controller_{ 60 } {
+GameMainLoop::GameMainLoop() :
+    keyboard_ptr_(std::make_shared<DxLibKeyboard>()),
+    mouse_ptr_(std::make_shared<DxLibMouse>()),
+    fps_controller_{ 60 },
+    scene_change_listener_ptr_(std::make_shared<SceneChangeListener>()),
+    scene_stack_ptr_(initializeSceneStack()),
+    scene_change_executer_{ scene_change_listener_ptr_, scene_stack_ptr_ } {
 }
 
-bool GameMainLoop::loop() {
+bool GameMainLoop::loop()
+{
+    //入力を取得
+    keyboard_ptr_->update();
+    mouse_ptr_->update();
 
-    // 入力を取得
-    Keyboard::getIns()->update();
-
-    // シーンのスタックの一番上を実行
-    if (!scene_manager_.updateTopScene()) {
+    // シーンのスタックの一番上を実行する．
+    if (!scene_stack_ptr_->UpdateTopScene())
+    {
         return false;
     }
 
-    if (!fps_controller_.skipDrawScene()) {
-
-        // 裏画面を初期化
-        if (ClearDrawScreen() < 0) {
+    // 処理が重い場合はここでコマ落ちさせる．
+    if (!fps_controller_.skipDrawScene())
+    {
+        // スクリーンを消す．
+        if (ClearDrawScreen() != 0)
+        {
             return false;
         }
 
-        scene_manager_.drawTopScene();
+        // 描画する．
+        scene_stack_ptr_->DrawTopScene();
 
-        // 裏画面を表画面に反映
-        if (ScreenFlip() < 0) {
+        // スクリーンに表示する．
+        if (ScreenFlip() != 0)
+        {
             return false;
         }
     }
 
-    // FPSを一定にするために待つ
+    // FPSを調整するための処理．
     fps_controller_.wait();
 
-    // 全処理の遂行後,シーンチェンジを行う
-    scene_manager_.sceneChangeExecute();
+    // シーンの変更を実行する．
+    scene_change_executer_.Execute();
 
     return true;
+}
+
+std::shared_ptr<SceneStack> GameMainLoop::initializeSceneStack() const
+{
+    ASSERT_NOT_NULL_PTR(scene_change_listener_ptr_);
+    ASSERT_NOT_NULL_PTR(keyboard_ptr_);
+    ASSERT_NOT_NULL_PTR(mouse_ptr_);
+
+    LanguageRecordInitializer language_record_initializer;
+    const std::shared_ptr<const LanguageRecord> language_record_ptr = std::make_shared<const LanguageRecord>(language_record_initializer.Initialize());
+
+    auto scene_creator_ptr = std::make_unique<SceneCreator>(
+        scene_change_listener_ptr_,
+        language_record_ptr,
+        keyboard_ptr_, mouse_ptr_,
+        std::make_shared<BgmPlayer>(), std::make_shared<FontLoader>(),
+        std::make_shared<ImageLoader>(), std::make_shared<SoundEffectLoader>());
+
+    auto scene_stack_ptr = std::make_shared<SceneStack>(std::move(scene_creator_ptr));
+
+    return scene_stack_ptr;
 }
 
 }  // namespace match_stick
--- MyProject/game_main_loop.h
+++ MyProject/game_main_loop.h
@@ -3,8 +3,12 @@
 
 #include <memory>
 
+#include "dxlib_keyboard.h"
+#include "dxlib_mouse.h"
 #include "fps_controller.h"
-#include "SceneManager.h"
+#include "scene_change_executer.h"
+#include "scene_change_listener.h"
+#include "scene_stack.h"
 
 namespace match_stick {
 
@@ -16,8 +20,16 @@ public:
     bool loop();
 
 private:
-    SceneManger scene_manager_;
+    std::shared_ptr<SceneStack> initializeSceneStack() const;
+
+    std::shared_ptr<DxLibKeyboard> keyboard_ptr_;
+    std::shared_ptr<DxLibMouse> mouse_ptr_;
+
     FpsController fps_controller_;
+
+    const std::shared_ptr<SceneChangeListener> scene_change_listener_ptr_;
+    const std::shared_ptr<SceneStack> scene_stack_ptr_;
+    SceneChangeExecuter scene_change_executer_;
 };
 
 }  // namespace match_stick
