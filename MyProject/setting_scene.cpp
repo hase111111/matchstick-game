162374fc3946a39c49b8c8e2b8a9d1bad99383d6
Hasegawa Taisei <hasehasehase61@gmail.com>
2024-08-17 23:28:02 +0900

    設定シーンを追加

---
+++ MyProject/setting_scene.cpp
@@ -0,0 +1,20 @@
+﻿
+#include "setting_scene.h"
+
+namespace match_stick {
+
+SettingScene::SettingScene(const std::shared_ptr<SceneChangeListener>& scene_change_listener_ptr,
+                           const std::shared_ptr<const FpsController>& entity_updater_ptr,
+                           const std::shared_ptr<const LanguageRecord>& language_record_ptr,
+                           const std::shared_ptr<const DxLibInput>& input_ptr,
+                           const std::shared_ptr<const FontLoader>& font_loader_ptr,
+                           const std::shared_ptr<const ImageLoader>& img_loader_ptr,
+                           const std::shared_ptr<const SoundEffectLoader>& sound_effect_loader_ptr) {}
+
+bool SettingScene::update() {
+    return true;
+}
+
+void SettingScene::draw() const {}
+
+}  // namespace match_stick
