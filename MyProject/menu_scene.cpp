6953328b55e6fd0102837987db525b189c863efb
hase111111 <hasehasehase61@gmail.com>
2024-09-02 14:58:48 +0900

    #12 menu_scene.cppの処理を関数にまとめた

--- MyProject/menu_scene.cpp
+++ MyProject/menu_scene.cpp
@@ -44,97 +44,8 @@ MenuScene::MenuScene(const std::shared_ptr<SceneChangeListener>& scene_change_li
         dxlib_input_ptr, dxlib_resource_loader_ptr));
     entity_updater_ptr_->registerEntity(std::make_shared<MenuBackGroundBase>());
 
-    // UI を構築
-    const auto dxlib_user_interface_base_ptr = std::make_shared<DxLibUserInterfaceBase>(dxlib_input_ptr);
-    entity_updater_ptr_->registerEntity(dxlib_user_interface_base_ptr);
-
-    constexpr int ui_start_x = GameConst::kResolutionX * 73 / 1000;
-    constexpr int ui_start_y = GameConst::kResolutionY / 27;
-    constexpr int ui_interval = GameConst::kResolutionX / 48;
-    constexpr int ui_width = GameConst::kResolutionX * 100 / 369;
-    constexpr int ui_height = GameConst::kResolutionY * 100 / 257;
-
-    const auto to_game_button_ptr = std::make_shared<MenuButton>(
-        language_record_ptr, dxlib_resource_loader_ptr,
-        ui_start_x + (ui_width * 2 + ui_interval) / 2, ui_start_y + ui_height / 2,
-        "data/img/icon_game.png", "menu_game",
-        [this]() {
-            // シーン遷移
-            scene_change_listener_ptr_->requestAddScene(SceneName::kDebug, SceneChangeParameter{});
-        },
-        true);
-    entity_updater_ptr_->registerEntity(to_game_button_ptr);
-    dxlib_user_interface_base_ptr->registerInterface(to_game_button_ptr, 0);
-
-    const auto to_rule_button_ptr = std::make_shared<MenuButton>(
-        language_record_ptr, dxlib_resource_loader_ptr,
-        ui_start_x + (ui_width + ui_interval) * 2 + ui_width / 2, ui_start_y + ui_height / 2,
-        "data/img/icon_rule.png", "menu_rule",
-        [this]() {
-            // シーン遷移
-            scene_change_listener_ptr_->requestAddScene(SceneName::kRule, SceneChangeParameter{});
-        });
-    entity_updater_ptr_->registerEntity(to_rule_button_ptr);
-    dxlib_user_interface_base_ptr->registerInterface(to_rule_button_ptr, 1);
-
-    const auto to_setting_button_ptr = std::make_shared<MenuButton>(
-        language_record_ptr, dxlib_resource_loader_ptr,
-        ui_start_x + ui_width / 2, ui_start_y + ui_height * 3 / 2 + ui_interval,
-        "data/img/icon_setting.png", "menu_setting",
-        [this]() {
-            // シーン遷移
-            scene_change_listener_ptr_->requestAddScene(SceneName::kSetting, SceneChangeParameter{});
-        });
-    entity_updater_ptr_->registerEntity(to_setting_button_ptr);
-    dxlib_user_interface_base_ptr->registerInterface(to_setting_button_ptr, 2);
-
-    const auto to_replay_button_ptr = std::make_shared<MenuButton>(
-        language_record_ptr, dxlib_resource_loader_ptr,
-        ui_start_x + (ui_width + ui_interval) + ui_width / 2, ui_start_y + ui_height * 3 / 2 + ui_interval,
-        "data/img/icon_replay.png", "menu_replay",
-        [this]() {
-            // ゲーム終了
-            game_end_ = true;
-        });
-    entity_updater_ptr_->registerEntity(to_replay_button_ptr);
-    dxlib_user_interface_base_ptr->registerInterface(to_replay_button_ptr, 3);
-
-    const auto to_language_button_ptr = std::make_shared<MenuButton>(
-        language_record_ptr, dxlib_resource_loader_ptr,
-        ui_start_x + (ui_width + ui_interval) * 2 + ui_width / 2, ui_start_y + ui_height * 3 / 2 + ui_interval,
-        "data/img/icon_language.png", "menu_language",
-        [this]() {
-            // シーン遷移
-            scene_change_listener_ptr_->requestAddScene(SceneName::kLanguage, SceneChangeParameter{});
-        });
-    entity_updater_ptr_->registerEntity(to_language_button_ptr);
-    dxlib_user_interface_base_ptr->registerInterface(to_language_button_ptr, 4);
-
-    constexpr int button_width = GameConst::kResolutionX * 5 / 24;
-    constexpr int button_height = GameConst::kResolutionY / 9;
-    constexpr int button_diff = GameConst::kResolutionX / 96;
-
-    const auto back_button_ptr = std::make_shared<SimpleBoxButton>(
-        language_record_ptr, dxlib_resource_loader_ptr,
-        GameConst::kResolutionX - button_width / 2 - button_diff,
-        GameConst::kResolutionY - button_height / 2 - button_diff,
-        button_width, button_height, "menu_back", "data/font/azuki_font24.dft",
-        [this]() {
-            sceneBackCallback();
-        });
-    entity_updater_ptr_->registerEntity(back_button_ptr);
-    dxlib_user_interface_base_ptr->registerInterface(back_button_ptr, 5);
-
-    const auto game_end_button_ptr = std::make_shared<SimpleBoxButton>(
-        language_record_ptr, dxlib_resource_loader_ptr,
-        GameConst::kResolutionX - button_width * 3 / 2 - button_diff * 2,
-        GameConst::kResolutionY - button_height / 2 - button_diff,
-        button_width, button_height, "menu_end", "data/font/azuki_font24.dft",
-        [this]() {
-            game_end_ = true;
-        });
-    entity_updater_ptr_->registerEntity(game_end_button_ptr);
-    dxlib_user_interface_base_ptr->registerInterface(game_end_button_ptr, 6);
+    // UI を初期化
+    initUI(language_record_ptr, dxlib_input_ptr, dxlib_resource_loader_ptr);
 
     // フェードイン演出を追加
     const auto fade_effect_ptr = std::make_shared<FadeEffect>(30, FadeEffect::FadeType::kFadeIn, []() {});
@@ -167,39 +78,125 @@ void MenuScene::onReturnFromOtherScene(const SceneChangeParameter&) {
     now_scene_change_ = false;
 }
 
-void MenuScene::sceneBackCallback() {
-    if (now_scene_change_) {
-        return;
-    }
+void MenuScene::initUI(
+    const std::shared_ptr<const LanguageRecord>& language_record_ptr,
+    const std::shared_ptr<const DxLibInput>& dxlib_input_ptr,
+    const std::shared_ptr<const DxLibResourceLoader>& dxlib_resource_loader_ptr) {
+    // ポインタが nullptr でないことを確認
+    ASSERT_NOT_NULL_PTR(language_record_ptr);
+    ASSERT_NOT_NULL_PTR(dxlib_resource_loader_ptr);
 
-    DEBUG_PRINT("Return the scene to the previous one.");
+    // UI に渡すコールバック関数を設定
+    const auto scene_change_callback = [this](const SceneName scene_name) {
+        if (now_scene_change_) {
+            return;
+        }
 
-    now_scene_change_ = true;
+        DEBUG_PRINT(std::format("Change the scene to {}.", magic_enum::enum_name(scene_name)));
 
-    // フェードアウト演出を追加
-    const auto fade_effect_ptr = std::make_shared<FadeEffect>(30, FadeEffect::FadeType::kFadeOut, [this]() {
-        scene_change_listener_ptr_->requestDeleteScene(1, SceneChangeParameter{});
-    });
+        now_scene_change_ = true;
 
-    entity_updater_ptr_->registerEntity(fade_effect_ptr);
-}
+        // フェードアウト演出を追加
+        const auto fade_effect_ptr = std::make_shared<FadeEffect>(
+            30, FadeEffect::FadeType::kFadeOut, [this, scene_name]() {
+                scene_change_listener_ptr_->requestAddScene(scene_name, SceneChangeParameter{});
+        });
 
-void MenuScene::sceneChangeCallback(const SceneName scene_name) {
-    if (now_scene_change_) {
-        return;
-    }
+        entity_updater_ptr_->registerEntity(fade_effect_ptr);
+    };
 
-    DEBUG_PRINT(std::format("Change the scene to {}.", magic_enum::enum_name(scene_name)));
+    // UI を構築
+    const auto dxlib_user_interface_base_ptr = std::make_shared<DxLibUserInterfaceBase>(dxlib_input_ptr);
+    entity_updater_ptr_->registerEntity(dxlib_user_interface_base_ptr);
 
-    now_scene_change_ = true;
+    constexpr int ui_start_x = GameConst::kResolutionX * 73 / 1000;
+    constexpr int ui_start_y = GameConst::kResolutionY / 27;
+    constexpr int ui_interval = GameConst::kResolutionX / 48;
+    constexpr int ui_width = GameConst::kResolutionX * 100 / 369;
+    constexpr int ui_height = GameConst::kResolutionY * 100 / 257;
 
-    // フェードアウト演出を追加
-    const auto fade_effect_ptr = std::make_shared<FadeEffect>(30, FadeEffect::FadeType::kFadeOut, [this, scene_name]() {
-        scene_change_listener_ptr_->requestAddScene(scene_name, SceneChangeParameter{});
-    });
+    const auto to_game_button_ptr = std::make_shared<MenuButton>(
+        language_record_ptr, dxlib_resource_loader_ptr,
+        ui_start_x + (ui_width * 2 + ui_interval) / 2, ui_start_y + ui_height / 2,
+        "data/img/icon_game.png", "menu_game",
+        [this, scene_change_callback]() { scene_change_callback(SceneName::kGame); },
+        true);
+    entity_updater_ptr_->registerEntity(to_game_button_ptr);
+    dxlib_user_interface_base_ptr->registerInterface(to_game_button_ptr, 0);
 
-    entity_updater_ptr_->registerEntity(fade_effect_ptr);
+    const auto to_rule_button_ptr = std::make_shared<MenuButton>(
+        language_record_ptr, dxlib_resource_loader_ptr,
+        ui_start_x + (ui_width + ui_interval) * 2 + ui_width / 2, ui_start_y + ui_height / 2,
+        "data/img/icon_rule.png", "menu_rule",
+        [this, scene_change_callback]() { scene_change_callback(SceneName::kRule); });
+    entity_updater_ptr_->registerEntity(to_rule_button_ptr);
+    dxlib_user_interface_base_ptr->registerInterface(to_rule_button_ptr, 1);
+
+    const auto to_setting_button_ptr = std::make_shared<MenuButton>(
+        language_record_ptr, dxlib_resource_loader_ptr,
+        ui_start_x + ui_width / 2, ui_start_y + ui_height * 3 / 2 + ui_interval,
+        "data/img/icon_setting.png", "menu_setting",
+        [this, scene_change_callback]() { scene_change_callback(SceneName::kSetting); });
+    entity_updater_ptr_->registerEntity(to_setting_button_ptr);
+    dxlib_user_interface_base_ptr->registerInterface(to_setting_button_ptr, 2);
+
+    const auto to_replay_button_ptr = std::make_shared<MenuButton>(
+        language_record_ptr, dxlib_resource_loader_ptr,
+        ui_start_x + (ui_width + ui_interval) + ui_width / 2, ui_start_y + ui_height * 3 / 2 + ui_interval,
+        "data/img/icon_replay.png", "menu_replay",
+        [this, scene_change_callback]() { scene_change_callback(SceneName::kReplay); });
+    entity_updater_ptr_->registerEntity(to_replay_button_ptr);
+    dxlib_user_interface_base_ptr->registerInterface(to_replay_button_ptr, 3);
+
+    const auto to_language_button_ptr = std::make_shared<MenuButton>(
+        language_record_ptr, dxlib_resource_loader_ptr,
+        ui_start_x + (ui_width + ui_interval) * 2 + ui_width / 2, ui_start_y + ui_height * 3 / 2 + ui_interval,
+        "data/img/icon_language.png", "menu_language",
+        [this, scene_change_callback]() { scene_change_callback(SceneName::kLanguage); });
+    entity_updater_ptr_->registerEntity(to_language_button_ptr);
+    dxlib_user_interface_base_ptr->registerInterface(to_language_button_ptr, 4);
+
+    constexpr int button_width = GameConst::kResolutionX * 5 / 24;
+    constexpr int button_height = GameConst::kResolutionY / 9;
+    constexpr int button_diff = GameConst::kResolutionX / 96;
+
+    const auto back_button_ptr = std::make_shared<SimpleBoxButton>(
+        language_record_ptr, dxlib_resource_loader_ptr,
+        GameConst::kResolutionX - button_width / 2 - button_diff,
+        GameConst::kResolutionY - button_height / 2 - button_diff,
+        button_width, button_height, "menu_back", "data/font/azuki_font24.dft",
+        [this]() {
+            if (now_scene_change_) {
+                return;
+            }
+
+            DEBUG_PRINT("Return the scene to the previous one.");
+
+            now_scene_change_ = true;
+
+            // フェードアウト演出を追加
+            const auto fade_effect_ptr = std::make_shared<FadeEffect>(30, FadeEffect::FadeType::kFadeOut, [this]() {
+                scene_change_listener_ptr_->requestDeleteScene(1, SceneChangeParameter{});
+            });
+
+            entity_updater_ptr_->registerEntity(fade_effect_ptr);
+        });
+    entity_updater_ptr_->registerEntity(back_button_ptr);
+    dxlib_user_interface_base_ptr->registerInterface(back_button_ptr, 5);
+
+    const auto game_end_button_ptr = std::make_shared<SimpleBoxButton>(
+        language_record_ptr, dxlib_resource_loader_ptr,
+        GameConst::kResolutionX - button_width * 3 / 2 - button_diff * 2,
+        GameConst::kResolutionY - button_height / 2 - button_diff,
+        button_width, button_height, "menu_end", "data/font/azuki_font24.dft",
+        [this]() {
+            game_end_ = true;
+        });
+    entity_updater_ptr_->registerEntity(game_end_button_ptr);
+    dxlib_user_interface_base_ptr->registerInterface(game_end_button_ptr, 6);
+
+    // UI の設定を初期化
+    dxlib_user_interface_base_ptr->setDefaultSelectedId(0);
 }
 
 }  // namespace match_stick
-
