bba0e12df6640de07479ceba90c0327ce0fe9bdf
hase111111 <hasehasehase61@gmail.com>
2024-09-05 21:57:53 +0900

    #8 SoundChangeBarを作成

---
+++ MyProject/sound_change_bar.cpp
@@ -0,0 +1,16 @@
+﻿
+#include "sound_change_bar.h"
+
+namespace match_stick {
+
+bool SoundChangeBar::update() {
+    return false;
+}
+
+void SoundChangeBar::draw() const {}
+
+bool SoundChangeBar::isHovered(int mouse_x, int mouse_y) const {
+    return false;
+}
+
+}  // match_stick
---
+++ MyProject/sound_change_bar.h
@@ -0,0 +1,33 @@
+﻿
+#pragma once
+
+#include "i_dxlib_user_interface.h"
+#include "i_scene.h"
+
+namespace match_stick {
+
+class SoundChangeBar final : public IDxLibUserInterface, public IScene {
+public:
+    SoundChangeBar(int x, int y, int default_value);
+    ~SoundChangeBar() = default;
+
+    bool update() override;
+
+    void draw() const override;
+
+    void onStart(const SceneChangeParameter&) override {};
+
+    void onReturnFromOtherScene(const SceneChangeParameter&) override {};
+
+    bool isHovered(int mouse_x, int mouse_y) const;
+
+    void initHoverState(bool is_hovered);
+
+    void callbackWhenClicked();
+
+    void callbackWhenHoverStarted();
+
+    void callbackWhenHoverEnded();
+};
+
+}  // match_stick
