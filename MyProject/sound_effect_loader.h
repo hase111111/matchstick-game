2b3dd09320a20170fdbf16709b98f6c22f73e299
hase111111 <hasehasehase61@gmail.com>
2024-09-12 22:12:29 +0900

    音量変更の関数を追加

--- MyProject/sound_effect_loader.h
+++ MyProject/sound_effect_loader.h
@@ -11,15 +11,32 @@ namespace match_stick {
 //! @brief SEを読み込むクラス．
 class SoundEffectLoader final {
 public:
+    //! @brief SEを読み込む．
+    //! 既に読み込んでいる場合は何もしない．
+    //! ファイルが存在しない場合はASSERTを発生させる．
+    //! @param file_path ファイルパス．
     void loadSoundHandle(const std::string& file_path);
 
+    //! @brief SEのハンドルを取得する．
+    //! @param file_path ファイルパス．
+    //! @return SEのハンドル．
     [[nodiscard]]
     inline int getSoundHandle(const std::string& file_path) const {
         ASSERT(sound_handle_.count(file_path) != 0, "The sound effect is not loaded. File is " + file_path);
         return sound_handle_.at(file_path);
     }
 
+    //! @brief 全てのSEの音量を変更する．
+    //! 読み込んだSEの音量を変更し，今後読み込むSEの音量も変更する．
+    //! @param volume 音量．
     void changeAllSoundVolume(int volume);
+
+    //! @brief SEの音量を取得する．
+    //! @return SEの音量．
+    [[nodiscard]]
+    inline int getVolumePercent() const {
+        return volume_percent_;
+    }
 
 private:
     [[nodiscard]]
